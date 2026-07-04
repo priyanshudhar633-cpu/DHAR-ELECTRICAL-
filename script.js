22a2a71cf44c50dab0c8bbaf155ffe834ea188ed
priyanshudhar633-cpu <priyanshudhar633@gmail.com>
2026-07-05 00:25:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Dhar Electrical Website
+console.log("Welcome to Dhar Electrical!");
+
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
